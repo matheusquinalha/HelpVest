c86dc8d20fd1159579292e62011d01ca33d3d769
Matheus Quinalha <matheusquinalha@gmail.com>
2020-05-14 22:16:46 -0300

    Atualização Activity's
    Foi retirado as duplicidades e arurmado as telas e strings

--- app/src/main/java/com/example/helpvest/CadastroActivity.java
+++ app/src/main/java/com/example/helpvest/CadastroActivity.java
@@ -2,30 +2,26 @@ package com.example.helpvest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< HEAD
 import android.os.Bundle;
-=======
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 
 import java.util.Objects;
->>>>>>> 9bee3d299dab3c2c46f3a0edb971f8bcce9bff05
+
 
 public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
+
         setContentView(R.layout.activity_cadastro);
-    }
-}
-=======
         Objects.requireNonNull(getSupportActionBar()).hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_cadastro);
     }
 }
->>>>>>> 9bee3d299dab3c2c46f3a0edb971f8bcce9bff05
+
--- app/src/main/java/com/example/helpvest/LoginActivity.java
+++ app/src/main/java/com/example/helpvest/LoginActivity.java
@@ -4,19 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-<<<<<<< HEAD
+
 import android.view.View;
 import android.widget.Button;
-=======
-import android.os.Handler;
-import android.view.View;
 import android.view.WindowManager;
 
 import java.util.Objects;
->>>>>>> 9bee3d299dab3c2c46f3a0edb971f8bcce9bff05
 
 public class LoginActivity extends AppCompatActivity {
-    private Button btnCadastrar,btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,22 +20,19 @@ public class LoginActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
-        this.btnCadastrar = findViewById(R.id.btnCadastrar);
+        Button btnCadastrar = findViewById(R.id.btnCadastrar);
 
-        this.btnCadastrar.setOnClickListener(new View.OnClickListener() {
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent mIntent = new Intent(LoginActivity.this, CadastroActivity.class);
                 startActivity(mIntent);
             }
         });
-
-
-
     }
         public void loginEntrar(View view){
 
-            Intent intent1 = new Intent(getApplicationContext(),LoginEntrarActivity.class);
+            Intent intent1 = new Intent(getApplicationContext(),MainActivity.class);
             startActivity(intent1);
         }
 
