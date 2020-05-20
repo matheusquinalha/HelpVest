d9ed24a1691e5b2c73c931a927e617e2dd5a3514
Matheus Quinalha <matheusquinalha@gmail.com>
2020-05-19 21:04:14 -0300

    Toolbar
    Inclusão da Toolbar ainda com problemas no menu 03 riscos

--- app/src/main/java/com/example/helpvest/SplashActivity.java
+++ app/src/main/java/com/example/helpvest/SplashActivity.java
@@ -14,7 +14,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Objects.requireNonNull(getSupportActionBar()).hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
